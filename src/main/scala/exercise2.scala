a977c199dd60867a1853f21b62fa190baf6efefe
Masahiro Honma <hiratara@cpan.org>
2012-03-25 14:14:53 +0900

    Solve exercise 2-4) (p.55)

--- src/main/scala/exercise2.scala
+++ src/main/scala/exercise2.scala
@@ -3,3 +3,17 @@ import hiratara.Statistics._
 object Exercise2_3 extends App {
   List(100, 500, 1000, 40000).foreach((n: Int) => println(sturgesRule(n)))
 }
+
+object Exercise2_4 extends App {
+  val min = scala.math.floor(5.18 * 10) / 10
+  val max = scala.math.floor(7.44 * (-10)) / (-10)
+  val n = 150
+  val range = max - min
+  val m = sturgesRule(n)
+  val r = (scala.math.round(range * 10.0 / m) / 10.0)
+  println(r)
+  for(i <- 0 to m - 1) {
+    println("" + (min + r * i) + " <= x < " + (min + r * (i + 1)))
+    println(min + r * (i + 0.5))
+  }
+}
