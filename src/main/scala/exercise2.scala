ca1b92acbbae88e0c8ba757ab5d7a5176bd238ef
Masahiro Honma <hiratara@cpan.org>
2012-03-25 14:26:17 +0900

    Solve exercise 2-5) (p. 55)
    Text book says that the start number of range should be 0.5,
    but it's too heuristic to solve automatically.

--- src/main/scala/exercise2.scala
+++ src/main/scala/exercise2.scala
@@ -17,3 +17,14 @@ object Exercise2_4 extends App {
     println(min + r * (i + 0.5))
   }
 }
+
+object Exercise2_5 extends App {
+  val min = scala.math.floor(0.72 * 10) / 10
+  val max = scala.math.floor(5.41 * (-10)) / (-10)
+  val range = max - min
+  val m = 10
+  val r = (scala.math.round(range * 10.0 / m) / 10.0)
+  for(i <- 0 to m - 1) {
+    println("" + (min + r * i) + " <= x < " + (min + r * (i + 1)))
+  }
+}
